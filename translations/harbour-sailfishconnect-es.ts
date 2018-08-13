b00629e9f80755a22df07af174c231b1cffe4223
GNU user <carlosgonz@protonmail.com>
2018-08-13 18:36:31 -0400

    Create  	harbour-sailfishconnect-es.ts

---
+++ translations/harbour-sailfishconnect-es.ts
@@ -0,0 +1,306 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>A KDE-Connect client for SailfishOS</source>
+        <translation>Un KDE-Connect cliente para SailfishOS</translation>
+    </message>
+    <message>
+        <source>Links</source>
+        <translation>Enlaces</translation>
+    </message>
+    <message>
+        <source>Licence</source>
+        <translation>Licencia:</translation>
+    </message>
+    <message>
+        <source>This program is free software: you can redistribute it and/or modifyit under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. &lt;br/&gt;&lt;br/&gt;This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.&lt;br/&gt;&lt;br/&gt;You should have received a copy of the GNU General Public License along with this program. If not, see &lt;a href=&quot;http://www.gnu.org/licenses/&quot;&gt;http://www.gnu.org/licenses/&lt;/a&gt;.</source>
+        <translation>Este es un programa libre: puedes redistribuirlo oh modificarlo bajo los términos de la licencia pública general de GNU publicado por la Free Software Foundation, ya sea la versión 3 de la licencia, o (a su opción) cualquier versión posterior. &lt;br/&gt;&lt;br/&gt;Este programa se distribuye con la esperanza de que será útil, pero sin ninguna garantía; sin ni siquiera la garantía implícita de comerciabilidad o idoneidad para un propósito en particular.  Para obtener más detalles, consulte la licencia pública general de GNU.&lt;br/&gt;&lt;br/&gt;debería haber recibido una copia de la licencia pública general GNU junto con este programa. Si no es así, ver &lt;a href="http://www.gnu.org/licenses/"&gt;http://www.gnu.org/licenses/&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Acercar de</translation>
+    </message>
+    <message>
+        <source>Bug reports on Github</source>
+        <translation>Reportar bicho en Github</translation>
+    </message>
+    <message>
+        <source>Source code on Github</source>
+        <translation>Código fuente en Github</translation>
+    </message>
+</context>
+<context>
+    <name>ClipboardUi</name>
+    <message>
+        <source>Copyboard</source>
+        <translation>Copiar al portapapeles</translation>
+    </message>
+    <message>
+        <source>After copying open the Sailfish Connect window to send the clipboard content. For receiving clipboard content this is not required.</source>
+        <translation>Después de copiar, abrir la ventana de conexión de Sailfish para enviar el contenido del portapapeles. Para recibir el contenido del portapapeles esto no esta requerido.</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Sailfish Connect</source>
+        <translation>Sailfish Connect</translation>
+    </message>
+</context>
+<context>
+    <name>Device</name>
+    <message>
+        <source>Already paired</source>
+        <translation>Ya está vinculado</translation>
+    </message>
+    <message>
+        <source>Device not reachable</source>
+        <translation>Dispositivo no accesible</translation>
+    </message>
+    <message>
+        <source>SHA1 fingerprint of your device certificate is: %1
+</source>
+        <translation>La huella digital SHA1 de su certificado de dispositivo es: %1</translation>
+    </message>
+    <message>
+        <source>SHA1 fingerprint of remote device certificate is: %1
+</source>
+        <translation>La huella digital SHA1 del certificado del dispositivo remoto es:% 1</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceListPage</name>
+    <message>
+        <source>Device Name</source>
+        <translation>Nombre del dispositivo</translation>
+    </message>
+    <message>
+        <source>Paired devices</source>
+        <translation>dispositivos vinculados</translation>
+    </message>
+    <message>
+        <source>Nearby devices</source>
+        <translation>Dispositivos cercanos</translation>
+    </message>
+    <message>
+        <source>Connected</source>
+        <translation>Conectado</translation>
+    </message>
+    <message>
+        <source>Pending pairing request ...</source>
+        <translation>Solicicitud  de vinculación pendientes...</translation>
+    </message>
+    <message>
+        <source>About ...</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>refrescar</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Sailfish Connect</source>
+        <translation>Sailfish Connect</translation>
+    </message>
+</context>
+<context>
+    <name>DevicePage</name>
+    <message>
+        <source>You don&apos;t trust this device</source>
+        <translation>Usted no confía en este dispositivo</translation>
+    </message>
+    <message>
+        <source>Request pairing</source>
+        <translation>Solicitar vinculacion</translation>
+    </message>
+    <message>
+        <source>Accept</source>
+        <translation>Aceptar</translation>
+    </message>
+    <message>
+        <source>Reject</source>
+        <translation>Rechazar</translation>
+    </message>
+    <message>
+        <source>You trust this device</source>
+        <translation>Usted confía en este dispositivo</translation>
+    </message>
+    <message>
+        <source>Waiting for accepted pairing ...</source>
+        <translation>Esperando vinculacion aceptada...</translation>
+    </message>
+    <message>
+        <source>This device wants to pair with your device.</source>
+        <translation>Este dispositivo quiere vincular con su dispositivo.</translation>
+    </message>
+    <message>
+        <source>Encryption info</source>
+        <translation>Información de encriptación</translation>
+    </message>
+    <message>
+        <source>Plugins</source>
+        <translation>Enchufes</translation>
+    </message>
+    <message>
+        <source>Unpair</source>
+        <translation>Desvincular</translation>
+    </message>
+    <message>
+        <source>Send ping</source>
+        <translation>Enviar una verificación</translation>
+    </message>
+</context>
+<context>
+    <name>DevicePluginsPage</name>
+    <message>
+        <source>Select Plugins</source>
+        <translation>Elegir enchufes</translation>
+    </message>
+</context>
+<context>
+    <name>EncryptionInfoPage</name>
+    <message>
+        <source>Encryption info</source>
+        <translation>Información de encriptación</translation>
+    </message>
+</context>
+<context>
+    <name>FileTransferJob</name>
+    <message>
+        <source>Receiving file over KDE Connect</source>
+        <translation>Recibiendo archivo a través de KDE-Connect</translation>
+    </message>
+    <message>
+        <source>Filename already present</source>
+        <translation>Nombre de archivo ya presente</translation>
+    </message>
+    <message>
+        <source>Received incomplete file: %1</source>
+        <translation>Archivo incompleto recibido: %1</translation>
+    </message>
+</context>
+<context>
+    <name>LanDeviceLink</name>
+    <message>
+        <source>This device cannot be paired because it is running an old version of KDE Connect.</source>
+        <translation>Este dispositivo no se puede vincular porque está ejecutando una versión antigua de KDE-Connect.</translation>
+    </message>
+</context>
+<context>
+    <name>LanPairingHandler</name>
+    <message>
+        <source>Canceled by other peer</source>
+        <translation>Cancelado por otro interlocutor</translation>
+    </message>
+    <message>
+        <source>%1: Already paired</source>
+        <translation>%1: Ya está vinculado</translation>
+    </message>
+    <message>
+        <source>Timed out</source>
+        <translation>Tiempo fuera</translation>
+    </message>
+</context>
+<context>
+    <name>MprisUi</name>
+    <message>
+        <source>No remote players</source>
+        <translation>No hay jugadores remotos</translation>
+    </message>
+    <message>
+        <source>Multimedia Controls</source>
+        <translation>Controles multimedia</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::AppDaemon</name>
+    <message>
+        <source>Pairing request from %1</source>
+        <translation>Solicitud de vinculacion desde %1</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::BatteryPluginFactory</name>
+    <message>
+        <source>Battery report</source>
+        <translation>reporte de batería</translation>
+    </message>
+    <message>
+        <source>Share battery charge state.</source>
+        <translation>compartir el estado de carga de la batería.</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::ClipboardPluginFactory</name>
+    <message>
+        <source>Clipboard</source>
+        <translation>Copiar al portapapeles</translation>
+    </message>
+    <message>
+        <source>Synchronize clipboard between devices.</source>
+        <translation>Sincroniza el portapapeles entre dispositivos.</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::MprisRemotePluginFactory</name>
+    <message>
+        <source>Multimedia controls</source>
+        <translation>Controles multimedia</translation>
+    </message>
+    <message>
+        <source>Control your media player from remote.</source>
+        <translation>Control de el reproductor multimedia desde el mando a distancia.</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::PingPluginFactory</name>
+    <message>
+        <source>Ping</source>
+        <translation>Ping</translation>
+    </message>
+    <message>
+        <source>Ping other device and receive pings.</source>
+        <translation>Ping otro dispositivo y recibir pings.</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::TelepathyPluginFactory</name>
+    <message>
+        <source>Send SMS</source>
+        <translation>Enviar SMS</translation>
+    </message>
+    <message>
+        <source>Allow sending of text messages.</source>
+        <translation>Permitir el envío de mensajes de texto.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Run in background</source>
+        <translation>Ejecutar en segundo plano</translation>
+    </message>
+    <message>
+        <source>Application runs in background after window is closed.</source>
+        <translation>La aplicación se ejecuta en segundo plano tras cerrar la ventana.</translation>
+    </message>
+</context>
+<context>
+    <name>UploadJob</name>
+    <message>
+        <source>Couldn&apos;t find an available port</source>
+        <translation>No se pudo encontrar un puerto disponible</translation>
+    </message>
+</context>
+</TS>

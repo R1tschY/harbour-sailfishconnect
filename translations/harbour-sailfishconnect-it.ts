70647a38a9d14611ab1e2b2f53e72411fb50668b
R1tschY <r1tschy@posteo.de>
2019-10-11 22:54:10 +0200

    Added Italian translation

---
+++ translations/harbour-sailfishconnect-it.ts
@@ -0,0 +1,541 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>Links</source>
+        <translation>Link</translation>
+    </message>
+    <message>
+        <source>Licence</source>
+        <translation>Licenza</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Info su</translation>
+    </message>
+    <message>
+        <source>Bug reports on Github</source>
+        <translation>Segnala bug su GitHub</translation>
+    </message>
+    <message>
+        <source>Source code on Github</source>
+        <translation>Codice sorgente su GitHub</translation>
+    </message>
+    <message>
+        <source>Alternative KDE Connect client for Sailfish OS</source>
+        <translation>Client KDE Connect per Sailfish OS</translation>
+    </message>
+</context>
+<context>
+    <name>ClipboardUi</name>
+    <message>
+        <source>Upload clipboard text</source>
+        <translation>Condividi appunti</translation>
+    </message>
+</context>
+<context>
+    <name>Device</name>
+    <message>
+        <source>Already paired</source>
+        <translation>Già connesso</translation>
+    </message>
+    <message>
+        <source>Device not reachable</source>
+        <translation>Dispositivo non raggiungibile</translation>
+    </message>
+    <message>
+        <source>SHA1 fingerprint of your device certificate is: %1
+</source>
+        <translation>L&apos;impronta SHA1 del certificato del tuo dispositivo è: %1
+</translation>
+    </message>
+    <message>
+        <source>SHA1 fingerprint of remote device certificate is: %1
+</source>
+        <translation>L&apos;impronta SHA1 del certificato del dispositivo remoto è: %1
+</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceListPage</name>
+    <message>
+        <source>Device Name</source>
+        <translation>Nome dispositivo</translation>
+    </message>
+    <message>
+        <source>Paired devices</source>
+        <translation>Dispositivi connessi</translation>
+    </message>
+    <message>
+        <source>Nearby devices</source>
+        <translation>Dispositivi vicini</translation>
+    </message>
+    <message>
+        <source>Connected</source>
+        <translation>Connesso</translation>
+    </message>
+    <message>
+        <source>Pending pairing request ...</source>
+        <translation>Richiesta connessione in attesa...</translation>
+    </message>
+    <message>
+        <source>About ...</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Ricarica</translation>
+    </message>
+    <message>
+        <source>Sailfish Connect</source>
+        <translation>Sailfish Connect</translation>
+    </message>
+    <message>
+        <source>Settings ...</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Esci</translation>
+    </message>
+    <message>
+        <source>Transfers ...</source>
+        <translation>Trasferimenti</translation>
+    </message>
+    <message>
+        <source>Install KDE Connect or GSConnect on your computer and connect it to the same WLAN.</source>
+        <translation>Installa KDE Connect o GSConnect sul tuo computer e connettilo alla stessa WLAN.</translation>
+    </message>
+</context>
+<context>
+    <name>DevicePage</name>
+    <message>
+        <source>Accept</source>
+        <translation>Accetta</translation>
+    </message>
+    <message>
+        <source>Reject</source>
+        <translation>Rifiuta</translation>
+    </message>
+    <message>
+        <source>This device wants to pair with your device.</source>
+        <translation>Questo dispositivo richiede una connessione con te.</translation>
+    </message>
+    <message>
+        <source>Encryption info</source>
+        <translation>Info crittografia</translation>
+    </message>
+    <message>
+        <source>Plugins</source>
+        <translation>Plugin</translation>
+    </message>
+    <message>
+        <source>Unpair</source>
+        <translation>Disaccoppia</translation>
+    </message>
+    <message>
+        <source>Send ping</source>
+        <translation>Invia ping</translation>
+    </message>
+    <message>
+        <source>Do you want to connect to this device?</source>
+        <translation>Vuoi connetterti a questo dispositivo?</translation>
+    </message>
+    <message>
+        <source>Connect</source>
+        <translation>Connetti</translation>
+    </message>
+    <message>
+        <source>Device is not reachable</source>
+        <translation>Dispositivo non raggiungibile</translation>
+    </message>
+    <message>
+        <source>Actions</source>
+        <translation>Azioni</translation>
+    </message>
+    <message>
+        <source>Waiting for accepted pairing ...</source>
+        <translation>In attesa che la richiesta sia accettata ...</translation>
+    </message>
+</context>
+<context>
+    <name>DevicePluginsPage</name>
+    <message>
+        <source>Select Plugins</source>
+        <translation>Seleziona plugin</translation>
+    </message>
+</context>
+<context>
+    <name>EncryptionInfoPage</name>
+    <message>
+        <source>Encryption info</source>
+        <translation>Info crittografia</translation>
+    </message>
+</context>
+<context>
+    <name>KeyboardChangeLayoutPage</name>
+    <message>
+        <source>%1ms repeat interval</source>
+        <translation>Intervallo di ripetizione %1ms</translation>
+    </message>
+    <message>
+        <source>Vibration feedback</source>
+        <translation>Vibrazione</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Change layout</source>
+        <translation>Cambia layout</translation>
+    </message>
+</context>
+<context>
+    <name>KeyboardPage</name>
+    <message>
+        <source>Keyboard</source>
+        <translation>Tastiera</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+</context>
+<context>
+    <name>LanDeviceLink</name>
+    <message>
+        <source>This device cannot be paired because it is running an old version of KDE Connect.</source>
+        <translation>Impossibile connettersi a questo dispositivo perché utilizza una vecchia versione di KDE Connect.</translation>
+    </message>
+    <message>
+        <source>This device cannot be paired because it sends a strange ssl certificate.</source>
+        <translation>Impossibile connettersi a questo dispositivo perché invia una certificato ssl non valido.</translation>
+    </message>
+</context>
+<context>
+    <name>LanPairingHandler</name>
+    <message>
+        <source>Canceled by other peer</source>
+        <translation>Annullato dall&apos;altro dispositivo</translation>
+    </message>
+    <message>
+        <source>%1: Already paired</source>
+        <translation>%1 già connesso</translation>
+    </message>
+    <message>
+        <source>Timed out</source>
+        <translation>Scaduto</translation>
+    </message>
+</context>
+<context>
+    <name>MprisUi</name>
+    <message>
+        <source>No remote players</source>
+        <translation>Nessun player remoto</translation>
+    </message>
+    <message>
+        <source>Multimedia Controls</source>
+        <translation>Controlli multimediali</translation>
+    </message>
+    <message>
+        <source>Nothing playing</source>
+        <translation>Niente in riproduzione</translation>
+    </message>
+</context>
+<context>
+    <name>RemoteKeyboard</name>
+    <message>
+        <source>Keyboard</source>
+        <translation>Tastiera</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::AppDaemon</name>
+    <message>
+        <source>Pairing request</source>
+        <translation>Richiesta connessione</translation>
+    </message>
+    <message>
+        <source>Pending pairing request ...</source>
+        <translation>Richiesta connessione in attesa...</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::BatteryPluginFactory</name>
+    <message>
+        <source>Battery report</source>
+        <translation>Stato batteria</translation>
+    </message>
+    <message>
+        <source>Share battery charge state.</source>
+        <translation>Condividi lo stato della batteria.</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::ClipboardPluginFactory</name>
+    <message>
+        <source>Clipboard</source>
+        <translation>Appunti</translation>
+    </message>
+    <message>
+        <source>Synchronize clipboard between devices.</source>
+        <translation>Sincronizza appunti tra i dispositivi.</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::CopyJob</name>
+    <message>
+        <source>Read error: %1</source>
+        <translation>Errore lettura: %1</translation>
+    </message>
+    <message>
+        <source>Write error: %1</source>
+        <translation>Errore scrittura: %1</translation>
+    </message>
+    <message>
+        <source>Input stream is not readable.</source>
+        <translation>Contenuto in arrivo non leggibile.</translation>
+    </message>
+    <message>
+        <source>Output stream is not writable.</source>
+        <translation>Contenuto in uscita non scrivibile.</translation>
+    </message>
+    <message>
+        <source>Read more bytes of input stream than expected.</source>
+        <translation>Contenuto in arrivo contiene più byte di quelli previsti.</translation>
+    </message>
+    <message>
+        <source>Early end of input stream</source>
+        <translation>Contenuto in arrivo chiuso anticipatamente</translation>
+    </message>
+    <message>
+        <source>Early end of output stream</source>
+        <translation>Contenuto in uscisa chiuso anticipatamente</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::DownloadJob</name>
+    <message>
+        <source>Cannot create destination folder</source>
+        <translation>Impossibile creare cartella di destinazione</translation>
+    </message>
+    <message>
+        <source>Could not open file for writing: %1</source>
+        <translation>Impossibile aprire file in scrittura: %1</translation>
+    </message>
+    <message>
+        <source>Receiving</source>
+        <translation>In ricezione</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::LanUploadJob</name>
+    <message>
+        <source>Couldn&apos;t find an available port</source>
+        <translation>Impossibile trovare una porta aperta</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::MprisRemotePluginFactory</name>
+    <message>
+        <source>Multimedia controls</source>
+        <translation>Controlli multimediali</translation>
+    </message>
+    <message>
+        <source>Control your media player from remote.</source>
+        <translation>Controlla il tuo player multimediale da remoto.</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::PingPluginFactory</name>
+    <message>
+        <source>Ping</source>
+        <translation>Ping</translation>
+    </message>
+    <message>
+        <source>Ping other device and receive pings.</source>
+        <translation>Invia ping all&apos;altro dispositivo o ricevi ping.</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::RemoteKeyboardPluginFactory</name>
+    <message>
+        <source>Remote Keyboard</source>
+        <translation>Tastiera remota</translation>
+    </message>
+    <message>
+        <source>Type on remote compute using a virtual keyboard.</source>
+        <translation>Scrivi sul computer remoto usando una tastiera virtuale.</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::SendNotificationsPluginFactory</name>
+    <message>
+        <source>Send notifcations</source>
+        <translation>Invia notifiche</translation>
+    </message>
+    <message>
+        <source>Work in progress: Send notifications to the remote computer.</source>
+        <translation>(In lavorazione) Invia notifiche al computer remoto.</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::SharePluginFactory</name>
+    <message>
+        <source>Share</source>
+        <translation>Condividi</translation>
+    </message>
+    <message>
+        <source>Send and receive files and URLs.</source>
+        <translation>Invia e ricevi file e URL.</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::TelepathyPluginFactory</name>
+    <message>
+        <source>Send SMS</source>
+        <translation>Invia SMS</translation>
+    </message>
+    <message>
+        <source>Allow sending of text messages.</source>
+        <translation>Permetti l&apos;invio di SMS.</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::TelephonyPluginFactory</name>
+    <message>
+        <source>Call notifications</source>
+        <translation>Notifiche chiamata</translation>
+    </message>
+    <message>
+        <source>Send notifications for incoming and missed calls.</source>
+        <translation>Invia notifiche per chiamate in arrivo o perse.</translation>
+    </message>
+</context>
+<context>
+    <name>SailfishConnect::TouchpadPluginFactory</name>
+    <message>
+        <source>Touchpad</source>
+        <translation>Touchpad</translation>
+    </message>
+    <message>
+        <source>Control remote computer through mouse movements and clicks.</source>
+        <translation>Controlla il computer remoto tramite mouse.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Run in background</source>
+        <translation>Esegui in background</translation>
+    </message>
+    <message>
+        <source>Application runs in background after window is closed.</source>
+        <translation>L&apos;app rimane in background dopo la chiusura della finestra.</translation>
+    </message>
+</context>
+<context>
+    <name>ShareUi</name>
+    <message>
+        <source>Share a file</source>
+        <translation>Invia un file</translation>
+    </message>
+    <message>
+        <source>Select file to send</source>
+        <translation>Seleziona file da inviare</translation>
+    </message>
+</context>
+<context>
+    <name>Touchpad</name>
+    <message>
+        <source>Touchpad</source>
+        <translation>Touchpad</translation>
+    </message>
+</context>
+<context>
+    <name>TouchpadPage</name>
+    <message>
+        <source>Touchpad</source>
+        <translation>Touchpad</translation>
+    </message>
+    <message>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Move finger on screen
+Tap for click
+Hold shortly for Drag&apos;n&apos;Drop</source>
+        <translation>Muovi dito sullo schermo
+Premi per click
+Tieni premuto per drag&apos;n&apos;drop</translation>
+    </message>
+</context>
+<context>
+    <name>TransfersPage</name>
+    <message>
+        <source>Transfers</source>
+        <translation>Trasferimenti</translation>
+    </message>
+    <message>
+        <source>Yet no downloads or uploads</source>
+        <translation>Ancora nessun file trasferito</translation>
+    </message>
+    <message>
+        <source>of</source>
+        <extracomment>Download progress, for example: 3MB of 50MB</extracomment>
+        <translation>di</translation>
+    </message>
+    <message>
+        <source>Failed to open file</source>
+        <translation>Impossibile aprire file</translation>
+    </message>
+    <message>
+        <source>Internal error</source>
+        <translation>Errore interno</translation>
+    </message>
+    <message>
+        <source>File does not exit</source>
+        <translation>File non esiste</translation>
+    </message>
+    <message>
+        <source>No program found to open file</source>
+        <translation>Nessuna app trovata per aprire il file</translation>
+    </message>
+    <message>
+        <source>xdg-open failed</source>
+        <translation>Errore xdg-open</translation>
+    </message>
+    <message>
+        <source>xdg-open crashed</source>
+        <translation>Crash xdg-open</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annulla</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Apri</translation>
+    </message>
+    <message>
+        <source>Canceled</source>
+        <translation>Annullato</translation>
+    </message>
+    <message>
+        <source>Failed</source>
+        <translation>Fallito</translation>
+    </message>
+    <message>
+        <source>Completed</source>
+        <translation>Completato</translation>
+    </message>
+</context>
+</TS>
